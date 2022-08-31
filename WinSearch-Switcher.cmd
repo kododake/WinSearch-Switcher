5493397d313db17a5694dbc6893e0bb434091a51
kododake <80098041+kododake@users.noreply.github.com>
2022-08-31 12:07:37 +0900

    Update WinSearch-Switcher.cmd

--- WinSearch-Switcher.cmd
+++ WinSearch-Switcher.cmd
@@ -5,17 +5,17 @@ if errorlevel 1 (
     PowerShell.exe -Command Start-Process \"%~f0\" -Verb runas
     exit
 )
-set /p cfm=サービスの状態を変更Start/Disable(s/d):
+set /p cfm=Change WindowsSearch status[Enable/Disable](s/d):
 if %cfm%==s (
-echo サービスを開始しました。
+echo Service has been started
 sc config "WSearch" start= auto
 sc start WSearch
 ) else if %cfm%==d (
-echo サービスを停止しました。
+echo Service has been stopped.
 sc stop WMPNetworkSvc
 sc stop WSearch
 sc config "WSearch" start= disabled
 ) else (
- echo キャンセルしました。
+ echo Canceled
   )
 pause
